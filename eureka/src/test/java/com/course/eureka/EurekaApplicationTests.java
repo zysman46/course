832254e6b6789c01a1be2e4247ab787a57cd1dcd
zysman46 <zysman46@gmail.com>
2022-02-10 16:02:10 +0800

    1.增加eureka模块作为注册服务端 2.将单maven项目改为maven父子项目

---
+++ eureka/src/test/java/com/course/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
